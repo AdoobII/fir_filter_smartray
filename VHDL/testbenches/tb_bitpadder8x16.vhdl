c01de5086c081176ef1de3b0675f0f085f3d13b8
Adib <adoobii25@gmail.com>
2021-09-13 01:39:27 +0200

    created a bit padding module that converts an 8-bit std_logic vector into a 16-bit signed vector

---
+++ VHDL/testbenches/tb_bitpadder8x16.vhdl
@@ -0,0 +1,59 @@
+LIBRARY ieee;
+USE ieee.numeric_std.ALL;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY tb_bitpadder8x16 IS
+END tb_bitpadder8x16;
+
+ARCHITECTURE behav OF tb_bitpadder8x16 IS
+    --component declaration
+    COMPONENT bitpadder8x16
+        PORT (
+            I : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            O : OUT SIGNED(15 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    --input signals
+    SIGNAL I : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+
+    --output signals
+    SIGNAL O : SIGNED(15 DOWNTO 0);
+
+BEGIN
+    --instantiation of bitpadder8x16
+    uut : bitpadder8x16 PORT MAP
+    (
+        I => I,
+        O => O
+    );
+
+    stim_proc : PROCESS
+    BEGIN
+        WAIT FOR 100 ns;
+        I <= x"00";
+        WAIT FOR 10 ns;
+        WAIT FOR 100 ns;
+        I <= x"01";
+        WAIT FOR 100 ns;
+        I <= x"01";
+        WAIT FOR 100 ns;
+        I <= x"03";
+        WAIT FOR 100 ns;
+        I <= x"03";
+        WAIT FOR 100 ns;
+        I <= x"ff";
+        WAIT FOR 100 ns;
+        I <= x"ff";
+        WAIT FOR 100 ns;
+        I <= x"ff";
+        I <= x"FE";
+        WAIT FOR 100 ns;
+        I <= x"00";
+        WAIT FOR 100 ns;
+        I <= x"04";
+        WAIT FOR 100 ns;
+        ASSERT false REPORT "Reached end of test";
+        WAIT;
+    END PROCESS;
+END behav;
